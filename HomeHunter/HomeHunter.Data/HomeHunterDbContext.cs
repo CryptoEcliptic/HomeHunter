6f912c2c08a8c9a29ffe4dc57b7b48df0650015d
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-06-26 17:35:39 +0300

    Added IAuditInfo interface and IdentityDbContext class

---
+++ HomeHunter/HomeHunter.Data/HomeHunterDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeHunter.Data
+{
+    public class HomeHunterDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public HomeHunterDbContext(DbContextOptions<HomeHunterDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
--- HomeHunter/HomeHunter.Domain/Common/BaseModel.cs
+++ HomeHunter/HomeHunter.Domain/Common/BaseModel.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
-namespace HomeHunter.Domain
+namespace HomeHunter.Domain.Common
 {
-    public class BaseModel<TKey>
+    public abstract class BaseModel<TKey> : IAuditInfo
     {
         [Key]
         public TKey Id { get; set; }
