66c8a9f40e976c36cffe6d865ab6a6df623238ee
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-07-03 21:55:48 +0300

    Refactored bootstrap template - logout functionality. Temporary shut down not verified email notification.

--- HomeHunter/HomeHunter.App/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ HomeHunter/HomeHunter.App/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HomeHunter.Domain;
+﻿using HomeHunter.Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace HomeHunter.App.Areas.Identity.Pages.Account
 {
@@ -23,8 +20,12 @@ namespace HomeHunter.App.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+
+            return Redirect("/");
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
