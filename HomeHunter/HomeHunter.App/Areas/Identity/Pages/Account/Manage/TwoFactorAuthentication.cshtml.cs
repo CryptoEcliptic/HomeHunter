cee7e3d468982ef8ea02d82cff6319224fae0620
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-07-05 16:43:16 +0300

    Repaired cookie consent policy alert

--- HomeHunter/HomeHunter.App/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
+++ HomeHunter/HomeHunter.App/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HomeHunter.Domain;
+﻿using HomeHunter.Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace HomeHunter.App.Areas.Identity.Pages.Account.Manage
 {
