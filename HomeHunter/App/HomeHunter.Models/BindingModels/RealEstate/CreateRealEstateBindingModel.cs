b475e201386410e94d13d4612c90add45b84069d
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-09-02 20:11:12 +0300

    Added RealEstateCreate binding model Error messages in separate constants.

--- HomeHunter/App/HomeHunter.Models/BindingModels/RealEstate/CreateRealEstateBindingModel.cs
+++ HomeHunter/App/HomeHunter.Models/BindingModels/RealEstate/CreateRealEstateBindingModel.cs
@@ -6,25 +6,37 @@ namespace HomeHunter.Models.BindingModels.RealEstate
 {
     public class CreateRealEstateBindingModel
     {
+        private const string RealEstateAreaRequirementMessage = "Площта не трябва да бъде по-малка от {1} и да надвишава {2}!";
+
+        private const string FieldIsRequiredMessage = "Полето {0} е задължително";
+        private const string PriceFieldRequirementMessage = "Цената не трябва да бъде по-малка от {1}.";
+
+        private const string AddressIsRequiredErrorMessage = "Полето Адрес/Местоположение е задължително";
+        private const string FieldLengthRequirementMessage = "Полето {0} трябва да бъде от поне {2} и да не надвишава {1} символа.";
+        private const string FloorDataRequirementsMessage = "Полето {0} не трябва да надвишава {1} символа.";
+
+        private const string TotalFloorsRequirementMessage = "Броят на етажите не трябва да бъде по-малък от {1} и да надвишава {2}.";
+
+
 
         [Display(Name = "Етаж")]
-        [StringLength(7, ErrorMessage = "Полето {0} не трябва да надвишава {1} символа.")]
+        [StringLength(7, ErrorMessage = FloorDataRequirementsMessage)]
         public string FloorNumber { get; set; }
 
 
         [Display(Name = "Брой етажи")]
-        [Range(1, 50, ErrorMessage = "Броят на етажите не трябва да бъде по-малък от {1}.")]
+        [Range(1, 50, ErrorMessage = TotalFloorsRequirementMessage)]
         public int? BuildingTotalFloors { get; set; }
 
-        [Required(ErrorMessage = "Полето {0} е задължително")]
-        [Display(Name = "Площ*")]
-        [Range(1, 1000000, ErrorMessage = "Площта не трябва да бъде по-малка от {1} и да надвишава {2}!")]
+        [Required(ErrorMessage = FieldIsRequiredMessage)]
+        [Display(Name = "Площ *")]
+        [Range(1, 1000000, ErrorMessage = RealEstateAreaRequirementMessage)]
         public double Area { get; set; }
 
         [Column(TypeName = "decimal(18,2)")]
-        [Required(ErrorMessage = "Полето Цена е задължително")]
-        [Display(Name = "Цена*")]
-        [Range(1, 9000000, ErrorMessage = "Цената не трябва да бъде по-малка от {1}.")]
+        [Required(ErrorMessage = FieldIsRequiredMessage)]
+        [Display(Name = "Цена *")]
+        [Range(1, 9000000, ErrorMessage = PriceFieldRequirementMessage)]
         public decimal Price { get; set; }
 
         [Display(Name = "Година")]
@@ -47,16 +59,16 @@ namespace HomeHunter.Models.BindingModels.RealEstate
         public bool CellingOrBasement { get; set; }
 
 
-        [Display(Name = "Адрес/Местоположение*")]
-        [Required(ErrorMessage = "Полето Адрес/Местоположение е задължително")]
-        [StringLength(256, ErrorMessage = "Полето {0} трябва да бъде от поне {2} и да не надвишава {1} символа.", MinimumLength = 3)]
+        [Display(Name = "Адрес/Местоположение *")]
+        [Required(ErrorMessage = AddressIsRequiredErrorMessage)]
+        [StringLength(256, ErrorMessage = FieldLengthRequirementMessage, MinimumLength = 3)]
         public string Address { get; set; }
 
         [MaxLength(32)]
         [Display(Name = "Вид отопление")]
         public string  HeatingSystem { get; set; }
 
-        [Required(ErrorMessage = "Полето {0} е задължително")]
+        [Required(ErrorMessage = FieldIsRequiredMessage)]
         [Display(Name = "Тип на имота *")]
         public string RealEstateType { get; set; }
 
@@ -69,7 +81,7 @@ namespace HomeHunter.Models.BindingModels.RealEstate
         public string City { get; set; }
 
         [Display(Name = "Село")]
-        [StringLength(32, ErrorMessage = "Полето {0} трябва да бъде от поне {2} и да не надвишава {1} символа.", MinimumLength = 3)]
+        [StringLength(32, ErrorMessage = FieldLengthRequirementMessage, MinimumLength = 3)]
         public string Village { get; set; }
 
         [MaxLength(64)]
