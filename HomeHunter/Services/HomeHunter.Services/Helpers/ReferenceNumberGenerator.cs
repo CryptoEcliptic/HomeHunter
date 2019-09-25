942a7d44cfaab482ee6bb2e3c2ed8927c1cd5ff7
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-09-25 09:38:12 +0300

    Removed disused Real estate types collections and oprimised ref number generator method

--- HomeHunter/Services/HomeHunter.Services/Helpers/ReferenceNumberGenerator.cs
+++ HomeHunter/Services/HomeHunter.Services/Helpers/ReferenceNumberGenerator.cs
@@ -3,7 +3,6 @@ using HomeHunter.Data;
 using HomeHunter.Domain.Enums;
 using HomeHunter.Services.Contracts;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,91 +10,49 @@ namespace HomeHunter.Services.Helpers
 {
     public class ReferenceNumberGenerator : IReferenceNumberGenerator
     {
-        private Dictionary<string, string> StartingCodesValues = new Dictionary<string, string>
-        {
-            { "Едностаен апартамент", "0001" },
-            { "Двустаен апартамент", "0200" },
-            { "Тристаен апартамент", "0300" },
-            { "Четиристаен апартамент", "0400" },
-            { "Многостаен апартамент", "0500" },
-            { "Мезонет", "0600" },
-            { "Ателие, Таван", "0700" },
-            { "Офис", "0800" },
-            { "Магазин", "0900" },
-            { "Заведение", "1000" },
-            { "Склад", "1100" },
-            { "Промишлено помещение", "1200" },
-            { "Етаж от къща", "1300" },
-            { "Къща", "1400" },
-            { "Гараж", "1500" },
-            { "Парцел", "1600" },
-            { "Земеделска земя", "1700" },
-            { "Хотел", "1800" },
-        };
-
-        private Dictionary<string, string> MaxCodeValues = new Dictionary<string, string>
-        {
-            { "Едностаен апартамент", "0199" },
-            { "Двустаен апартамент", "0299" },
-            { "Тристаен апартамент", "0399" },
-            { "Четиристаен апартамент", "0499" },
-            { "Многостаен апартамент", "0599" },
-            { "Мезонет", "0699" },
-            { "Ателие, Таван", "0799" },
-            { "Офис", "0899" },
-            { "Магазин", "0999" },
-            { "Заведение", "1099" },
-            { "Склад", "1199" },
-            { "Промишлено помещение", "1299" },
-            { "Етаж от къща", "1399" },
-            { "Къща", "1499" },
-            { "Гараж", "1599" },
-            { "Парцел", "1699" },
-            { "Земеделска земя", "1799" },
-            { "Хотел", "1899" },
-        };
-
         private const string StartSaleRefNumberDigit = "30";
         private const string StartRentRefNumberDigit = "10";
         private const int SymbolsToSkip = 2;
        
         private readonly HomeHunterDbContext context;
         private readonly IRealEstateServices realEstateServices;
+        private readonly IRealEstateTypeServices realEstateTypeServices;
 
         public ReferenceNumberGenerator(
             IRealEstateServices realEstateServices,
+            IRealEstateTypeServices realEstateTypeServices,
             HomeHunterDbContext context)
         {
             this.context = context;
             this.realEstateServices = realEstateServices;
+            this.realEstateTypeServices = realEstateTypeServices;
         }
 
         public async Task<string> GenerateOfferId(string offerType, string estateId)
         {
             var realEstate = await this.realEstateServices.GetDetailsAsync(estateId);
-            var estateType = realEstate.RealEstateType;
+            var realEstateType = await this.realEstateTypeServices.GetRealEstateTypeByNameAsync(realEstate.RealEstateType);
 
-            string referenceNumber = offerType == GlobalConstants.OfferTypeSaleName ? StartSaleRefNumberDigit : StartRentRefNumberDigit;
+            var currentReferenceNumber = offerType == GlobalConstants.OfferTypeSaleName ? StartSaleRefNumberDigit : StartRentRefNumberDigit;
 
-            var previousOfferRefNumber = await this.GetRefNumberOfLastOfferByEstateType(offerType, estateType);
-            string lastDigitsOfPreviousNumber = previousOfferRefNumber != null ? previousOfferRefNumber.Skip(SymbolsToSkip).ToString() : null;
+            var previousReferenceNumber = await this.GetLastReferenceNumber(offerType, realEstate.RealEstateType);
+            string lastDigitsOfPreviousReferenceNumber = previousReferenceNumber != null ? previousReferenceNumber.Skip(SymbolsToSkip).ToString() : null;
 
-            if (previousOfferRefNumber == null || MaxCodeValues[estateType] == lastDigitsOfPreviousNumber)
+            if (previousReferenceNumber == null || realEstateType.MaxReferenceNumber == lastDigitsOfPreviousReferenceNumber)
             {
-                referenceNumber += StartingCodesValues[estateType];
-                return referenceNumber;
+                currentReferenceNumber += realEstateType.MinReferenceNumber;
+                return currentReferenceNumber;
             }
             
             else
             {
-                int currentRefNumberAsInt = int.Parse(previousOfferRefNumber) + 1;
-                referenceNumber = currentRefNumberAsInt.ToString();
-                return referenceNumber;
+                int currentRefNumberAsInt = int.Parse(previousReferenceNumber) + 1;
+                currentReferenceNumber = currentRefNumberAsInt.ToString();
+                return currentReferenceNumber;
             }
-
         }
 
-        private async Task<string> GetRefNumberOfLastOfferByEstateType(string offerType, string estateType)
+        private async Task<string> GetLastReferenceNumber(string offerType, string estateType)
         {
             OfferType parsedEnum = offerType == GlobalConstants.OfferTypeSaleName ? OfferType.Sale : OfferType.Rental;
 
